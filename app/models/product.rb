3b4cab1f3cded4dd24cf11a18865d121513034ea
yangmengqing <ymg_best@163.com>
2015-06-08 16:47:48 +0800

    Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
   validates :image_url, :format => {
-    :with => %r{\.(gif|jpg|png)$}i,
+    :with => %r{\.(gif|jpg|png)\z}i,
     :message => 'must be a url for gif, jpg or png image.'
   }
 end
