c222dbc0e357a4d3de2ec3c28911f0cbdc8b11e3
yangmengqing <ymg_best@163.com>
2015-06-10 18:04:00 +0800

    add data relationship

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ActiveRecord::Base
   default_scope { order (:title) }
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true, length: {minimum: 10, message: 'must be longer than 10 characters'}
@@ -7,4 +10,16 @@ class Product < ActiveRecord::Base
     :with => %r{\.(gif|jpg|png)\z}i,
     :message => 'must be a url for gif, jpg or png image.'
   }
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
+
 end
