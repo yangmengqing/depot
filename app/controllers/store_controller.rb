2c42dc5c77fa00fb98b14451e5c36b9ffe57d008
yangmengqing <ymg_best@163.com>
2015-06-11 18:10:15 +0800

    add product to cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,11 @@ class StoreController < ApplicationController
   def index
     @products = Product.all
     @time = Time.now.strftime("%I:%M %p")
+    
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
   end
 end
