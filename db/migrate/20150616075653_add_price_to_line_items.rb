f573412dd87792f63e6a096d6a6bd92038c33403
yangmengqing <ymg_best@163.com>
2015-06-16 18:13:50 +0800

    task E end

---
+++ db/migrate/20150616075653_add_price_to_line_items.rb
@@ -0,0 +1,11 @@
+class AddPriceToLineItems < ActiveRecord::Migration
+  def change
+    add_column :line_items, :price, :decimal, :precision => 8, :scale => 2
+
+    say_with_time 'Updating prices...' do
+      LineItem.all.each do |lineitem|
+        lineitem.update_attribute :price, lineitem.product.price
+      end
+    end
+  end
+end
