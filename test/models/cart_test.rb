bb023213f4a01398a54f8f5c8c33eee42a062870
yangmengqing <ymg_best@163.com>
2015-06-17 16:14:20 +0800

    modify cart test file

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -4,4 +4,26 @@ class CartTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  #def new_cart_with_one_product(product)
+  #  cart = Cart.create
+  #  cart.add_product(product.id, product.price)
+  #  cart
+  #end
+
+  #test 'cart should create a new line item when adding a new product' do
+  #  cart = new_cart_with_one_product(products(:one))
+  #  assert_equal 1, cart.line_items.length
+  #  
+  #  cart.add_product(products(:ruby).id, products(:ruby).price)
+  #  assert_equal 2, cart.line_items.length
+  #end
+
+  #test 'cart should update an existing line item when adding an existing product' do
+  #  product = products(:ruby)
+  #  cart = new_cart_with_one_product(product)
+  #  cart.add_product(product.id, product.price)
+  #  assert_equal 1, cart.line_items.length
+  #end
+
 end
