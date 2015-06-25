d162c1784c2c9aa0389878888e29fef1666b7a2f
yangmengqing <ymg_best@163.com>
2015-06-25 18:02:19 +0800

    task h

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,15 +1,17 @@
 class Notifier < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "ymg_best@163.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.notifier.order_received.subject
   #
-  def order_received
-    @greeting = "Hi"
+  def order_received(order)
+    @order = order
 
-    mail to: "to@example.org"
+    mail(to: order.email, subject: 'Pragmatic Store Order Confirmation') do |format|
+      format.text
+    end
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -17,9 +19,9 @@ class Notifier < ActionMailer::Base
   #
   #   en.notifier.order_shipped.subject
   #
-  def order_shipped
-    @greeting = "Hi"
+  def order_shipped(order)
+    @order = order
 
-    mail to: "to@example.org"
+    mail to: order.email, subject: 'Pragmatic Store Order Shipped'
   end
 end
