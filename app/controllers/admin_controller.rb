84f3227c1d90b2afd58400eef1fddb6135f5aedc
yangmengqing <ymg_best@163.com>
2016-06-12 14:19:16 +0800

    test

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,6 @@
 class AdminController < ApplicationController
   def index
-    #@total_orders = Order.count
-    total_orders = Order.count
+    @total_orders = Order.count.size
   end
 
   def for_test
